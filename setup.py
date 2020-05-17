80d1206b515e63730076eefb199b9f83efb5b35e
Dmitry K <dmitrykalyukov@gmail.com>
2020-05-17 11:47:48 +0200

    Create empty project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="smth",
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux"
+    ],
+    python_requires='>=3.8',
+)
