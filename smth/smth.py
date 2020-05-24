38abf1c5b90643fcfa86be8ca23ef7bf19e22426
Dmitry K <dmitrykalyukov@gmail.com>
2020-05-24 19:50:16 +0200

    Set up logging

--- smth/smth.py
+++ smth/smth.py
@@ -1,2 +1,13 @@
+import logging
+
 def main():
-    print('Hello, World!')
+    setup_logging()
+
+def setup_logging(filename='smth.log', log_level=logging.DEBUG) -> None:
+    log = logging.getLogger()
+    log.setLevel(log_level)
+    handler = logging.FileHandler(filename)
+    handler.setLevel(log_level)
+    handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+    log.addHandler(handler)
+
