553f079886367a7207846b07723812c161374377
Dmitry K <dmitrykalyukov@gmail.com>
2020-05-25 10:42:20 +0200

    Print errors to stderr

--- smth/backup_system.py
+++ smth/backup_system.py
@@ -12,15 +12,16 @@ class BackupSystem:
         try:
             self._db = DB()
         except Exception as e:
-            print(f'Failed to initialize the database: {e}.')
-            log.exception('Failed to initialize the database')
+            self._print_exception('Failed to initialize the database', e)
             sys.exit(1)
 
         try:
             self._notebooks = self._db.get_notebooks()
         except Exception as e:
-            print(f'Failed to get notebooks from the database: {e}.')
-            log.exception('Failed to get notebooks from the database')
+            self._print_exception('Failed to get notebooks from database', e)
             sys.exit(1)
 
+    def _print_exception(self, message, e):
+        print(f'{message}: {e}.', file=sys.stderr)
+        log.exception(message)
 
