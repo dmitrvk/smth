fc9c1aa116d0ac80090c6e74348f1a3db21b32cf
Dmitry K <dmitrykalyukov@gmail.com>
2020-07-02 13:46:02 +0200

    Perform drive auth
    Ask user to visit a link and paste the verification code.
    Once done, credentials are saved in the config directory.
    Next time existing credentials are used.

--- smth/commands/upload.py
+++ smth/commands/upload.py
@@ -1,4 +1,6 @@
+import json
 import logging
+import pathlib
 from typing import List
 
 from smth import commands, db
@@ -7,15 +9,30 @@ log = logging.getLogger(__name__)
 
 
 class UploadCommand(commands.Command):  # pylint: disable=too-few-public-methods  # noqa: E501
-    """A command which can be executed with arguments."""
+    """A command for uploading notebooks to Google Drive."""
+
+    CLIENT_SECRETS = {
+        "installed": {
+            "client_id": "393847868490-0nbggpkeq4vn47050f2b10blmghp1uo7.apps.googleusercontent.com",  # noqa: E501
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "client_secret": "UgLqgONjhpvbMEuhUbIdVzub",
+            "redirect_uris": [
+                "urn:ietf:wg:oauth:2.0:oob",
+                "http://localhost",
+            ],
+        },
+    }
+
+    CLIENT_SECRETS_PATH = pathlib.Path(
+        '~/.config/smth/client_secrets.json').expanduser()
+
+    CREDENTIALS_PATH = pathlib.Path(
+        '~/.config/smth/credentials.json').expanduser()
 
     def execute(self, args: List[str] = None):
         """Upload notebook's PDF file to Google Drive."""
-        try:
-            import pydrive.auth  # noqa: F401
-            import pydrive.drive  # noqa: F401
-        except ImportError:
-            self._exit_with_error('PyDrive not found.')
+        self._auth()
 
         try:
             notebooks = self._db.get_notebook_titles()
@@ -31,3 +48,22 @@ class UploadCommand(commands.Command):  # pylint: disable=too-few-public-methods
                 self.view.show_info('This feature will be implemented in future versions')  # noqa: E501
         else:
             self.view.show_info('No notebooks found.')
+
+    def _auth(self):
+        try:
+            import pydrive.auth
+        except ImportError:
+            self._exit_with_error('PyDrive not found.')
+
+        gauth = pydrive.auth.GoogleAuth()
+        gauth.settings['client_config_file'] = str(self.CLIENT_SECRETS_PATH)
+
+        if not self.CLIENT_SECRETS_PATH.exists():
+            with open(str(self.CLIENT_SECRETS_PATH), 'w') as secrets_file:
+                json.dump(self.CLIENT_SECRETS, secrets_file)
+
+        if self.CREDENTIALS_PATH.exists():
+            gauth.LoadCredentialsFile(str(self.CREDENTIALS_PATH))
+        else:
+            gauth.CommandLineAuth()
+            gauth.SaveCredentialsFile(str(self.CREDENTIALS_PATH))
--- tests/test_upload.py
+++ tests/test_upload.py
@@ -1,16 +1,25 @@
 import logging
-import unittest
 from unittest import mock
 
-from smth import commands, db, models
+from pyfakefs import fake_filesystem_unittest
+
+from smth import commands, db, config, models
 
 
-class UploadCommandTestCase(unittest.TestCase):
+class UploadCommandTestCase(fake_filesystem_unittest.TestCase):
     def setUp(self):
+        self.setUpPyfakefs(modules_to_reload=[commands, config])
+        self.fs.create_dir(str(config.Config.CONFIG_PATH.parent))
+
         logging.disable()
 
         self.db = mock.MagicMock()
         self.view = mock.MagicMock()
+
+        self.gauth = mock.MagicMock()
+        auth_patcher = mock.patch('pydrive.auth.GoogleAuth')
+        auth_patcher.start().return_value = self.gauth
+        self.addCleanup(auth_patcher.stop)
 
     def test_execute(self):
         self.db.get_notebook_titles.return_value = ['notebook']
@@ -23,6 +32,27 @@ class UploadCommandTestCase(unittest.TestCase):
         commands.UploadCommand(self.db, self.view).execute()
 
         self.view.ask_for_notebook.assert_called_once()
+
+    def test_execute_auth_no_credentials(self):
+        secrets_path = str(commands.UploadCommand.CLIENT_SECRETS_PATH)
+        self.assertFalse(self.fs.exists(secrets_path))
+
+        commands.UploadCommand(self.db, self.view).execute()
+
+        self.assertTrue(self.fs.exists(secrets_path))
+
+        self.gauth.CommandLineAuth.assert_called_once()
+        self.gauth.SaveCredentialsFile.assert_called_once()
+
+    def test_execute_auth_with_credentials(self):
+        with mock.patch(
+                'smth.commands.UploadCommand.CREDENTIALS_PATH') as PATH:
+            PATH.exists.return_value = True
+
+            commands.UploadCommand(self.db, self.view).execute()
+
+        self.gauth.CommandLineAuth.assert_not_called()
+        self.gauth.LoadCredentialsFile.assert_called_once()
 
     def test_execute_db_error(self):
         self.db.get_notebook_titles.side_effect = db.Error('Failed')
