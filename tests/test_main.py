90b86ebf582e80057320e062e6e7b9288f0a9192
Dmitry K <dmitrykalyukov@gmail.com>
2020-07-29 12:42:38 +0200

    Refactor main module tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,115 +19,55 @@ class MainTestCase(fake_filesystem_unittest.TestCase):
         db_patcher.start().return_value = mock.MagicMock()
         self.addCleanup(db_patcher.stop)
 
+        self.commands = [
+            'create', 'delete', 'list', 'open', 'scan', 'share', 'types',
+            'update', 'upload',
+        ]
+
     @mock.patch('smth.main')
     def test__main__(self, mock):
         from smth import __main__  # noqa: F401  # Cover __main__.py with tests
 
-    def test_list_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'list']):
-            with mock.patch('smth.commands.ListCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
+    def test_commands_execution(self):
+        for command in self.commands:
+            with mock.patch.object(sys, 'argv', ['', command]):
+                command_class = f'smth.commands.{command.capitalize()}Command'
 
-    def test_open_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'open']):
-            with mock.patch('smth.commands.OpenCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
+                with mock.patch(command_class) as Command:
+                    command = mock.MagicMock()
+                    Command.return_value = command
+                    main.main()
+                    command.execute.assert_called_once()
 
-    def test_scan_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'scan']):
+    def test_pydrive_not_found(self):
+        for command in ['share', 'upload']:
+            with mock.patch.object(sys, 'argv', ['', command]):
+                command_class = f'smth.commands.{command.capitalize()}Command'
+
+                with mock.patch(command_class) as Command:
+                    command = mock.MagicMock()
+                    Command.return_value = command
+
+                    with mock.patch('importlib.util.find_spec') as find_spec:
+                        find_spec.return_value = None
+
+                        testutils.capture_stdout(main.main)
+
+                        command.execute.assert_not_called()
+
+    def test_default_command(self):
+        with mock.patch.object(sys, 'argv', ['__main__.py']):
             with mock.patch('smth.commands.ScanCommand') as Command:
                 command = mock.MagicMock()
                 Command.return_value = command
                 main.main()
                 command.execute.assert_called_once()
 
-    def test_share_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'share']):
-            with mock.patch('smth.commands.ShareCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
-
-    def test_share_command_pydrive_not_found(self):
-        with mock.patch.object(sys, 'argv', ['', 'share']):
-            with mock.patch('smth.commands.ShareCommand') as Command:
-                with mock.patch('importlib.util.find_spec') as find_spec:
-                    command = mock.MagicMock()
-                    Command.return_value = command
-                    find_spec.return_value = None
-                    testutils.capture_stdout(main.main)
-                    command.execute.assert_not_called()
-
-    def test_types_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'types']):
-            with mock.patch('smth.commands.TypesCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
-
-    def test_create_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'create']):
-            with mock.patch('smth.commands.CreateCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
-
-    def test_delete_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'delete']):
-            with mock.patch('smth.commands.DeleteCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
-
-    def test_update_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'update']):
-            with mock.patch('smth.commands.UpdateCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
-
-    def test_upload_command(self):
-        with mock.patch.object(sys, 'argv', ['', 'upload']):
-            with mock.patch('smth.commands.UploadCommand') as Command:
-                command = mock.MagicMock()
-                Command.return_value = command
-                main.main()
-                command.execute.assert_called_once()
-
-    def test_upload_command_pydrive_not_found(self):
-        with mock.patch.object(sys, 'argv', ['', 'upload']):
-            with mock.patch('smth.commands.UploadCommand') as Command:
-                with mock.patch('importlib.util.find_spec') as find_spec:
-                    command = mock.MagicMock()
-                    Command.return_value = command
-                    find_spec.return_value = None
-                    testutils.capture_stdout(main.main)
-                    command.execute.assert_not_called()
-
-    @mock.patch.object(sys, 'argv', ['__main__.py', 'test'])
     def test_unknown_command(self):
-        output = testutils.capture_stdout(main.main)
-        for command in ['create', 'list', 'scan', 'types']:
-            self.assertIn(command, output)
-
-    @mock.patch.object(sys, 'argv', ['__main__.py'])
-    def test_default_command(self):
-        with mock.patch('smth.commands.ScanCommand') as Command:
-            command = mock.MagicMock()
-            Command.return_value = command
-            main.main()
-            command.execute.assert_called_once()
+        with mock.patch.object(sys, 'argv', ['__main__.py', 'test']):
+            output = testutils.capture_stdout(main.main)
+            for command in self.commands:
+                self.assertIn(command, output)
 
     def test_config_error(self):
         with mock.patch('smth.config.Config') as Config:
